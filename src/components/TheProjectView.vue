a8a2c90c6c2f0e1f3fa0076d50bf1945db063c12
luwenrui <1365240681@qq.com>
2024-08-04 21:15:57 +0800

    chore: Technical

--- src/components/TheProjectView.vue
+++ src/components/TheProjectView.vue
@@ -4,6 +4,7 @@ import baoliwei from '~/static/baoliwei.png'
 import yangfan from '~/static/yangfan.png'
 import jixing from '~/static/jixing.png'
 import waiguoyu from '~/static/waiguoyu.png'
+import boming from '~/static/boming.png'
 
 const project = [
   {
@@ -14,9 +15,9 @@ const project = [
 
   {
     projeceName: '福建扬帆计划',
-    link: 'http://mp.zhisland.com/router/conference/',
+
     previewImg: yangfan,
-    projectDesc: '福建在线求职平台。',
+    projectDesc: '福建在线求职平台',
   },
 
   {
@@ -37,6 +38,11 @@ const project = [
     previewImg: zhenghedao,
     projectDesc: '正和岛企业家签到，活动流程展示平台。',
   },
+  {
+    projeceName: '博明小管家',
+    previewImg: boming,
+    projectDesc: '面向社会用户的第三方官方售药平台',
+  },
 ]
 </script>
 
@@ -45,11 +51,11 @@ const project = [
     <div grid cols-1 gap-6 md:cols-3>
       <div v-for="(item, index) in project" :key="index" rd-2 p-4 class="bg-[#373738]">
         <img :src="item.previewImg" m-auto block rd-2 style="object-fit: contain;">
-        <div ml3>
+        <div>
           <p my4 text-center text-lg>
             {{ item.projeceName }}
           </p>
-          <p mt2 text-sm op50>
+          <p mt2 text-center text-sm op50>
             {{ item.projectDesc }}
           </p>
         </div>
--- src/components/TheTechnology.vue
+++ src/components/TheTechnology.vue
@@ -8,8 +8,8 @@
         <h3 text-2xl font-bold>
           Chrome
         </h3>
-        <p mt2 text-base italic op50>
-          熟练使用 developer mode tools，用于调试、性能分析和开发测试.
+        <p mt2 text-base lh-8 italic op50>
+          熟练使用 Chrome 开发者模式，包括 Elements、Console、Network、Performance 等工具，用于调试、性能分析和开发测试。能够利用 Lighthouse 进行网站性能优化，并使用 DevTools Protocol 进行自动化测试和脚本调试。
         </p>
       </div>
       <div mt-8>
@@ -17,7 +17,7 @@
           H5、CSS3
         </h3>
         <p mt2 text-base lh-8 italic op50>
-          致力于网页的语义化设计和多样化视觉效果，深入理解并运用CSS盒子模型，实现高效响应式布局，确保在不同设备和屏幕尺寸上的兼容性和用户体验.
+          熟练使用 HTML5 和 CSS3，致力于网页的语义化设计和多样化视觉效果，深入理解并运用 CSS 盒子模型，实现高效响应式布局。熟悉 Flexbox 和 Grid 布局模型，能够确保在不同设备和屏幕尺寸上的兼容性和用户体验。掌握 CSS 预处理器如 Sass 和 Less，并能使用 PostCSS 进行样式处理。
         </p>
       </div>
 
@@ -25,24 +25,32 @@
         <h3 text-2xl font-bold>
           Wechat MiniPrograms
         </h3>
-        <p mt2 text-base italic op50>
-          uni-app多端开发框架，能够高效地为不同平台（包括为微信，字节，支付宝小程序、Web等）构建和优化应用.
+        <p mt2 text-base lh-8 italic op50>
+          熟悉原生微信小程序开发逻辑和核心概念，包括页面和组件的生命周期管理、数据绑定和事件处理等。熟练使用微信小程序 API，如网络请求、数据存储、文件上传下载、用户授权和支付等。能够进行性能优化，使用微信开发者工具进行调试。
+        </p>
+      </div>
+      <div mt-8>
+        <h3 text-2xl font-bold>
+          Uni-app
+        </h3>
+        <p mt2 text-base lh-8 italic op50>
+          熟练使用 uni-app 多端开发框架，能够高效地为不同平台（包括微信、字节跳动、支付宝小程序、Web 等）构建和优化应用。掌握 uni-app 的页面生命周期、组件和 API，能够处理复杂的跨平台兼容性问题，并且了解 HBuilderX 开发工具的使用和调试。
         </p>
       </div>
       <div mt-8>
         <h3 text-2xl font-bold>
           Pack
         </h3>
-        <p mt2 text-base italic op50>
-          熟练使用Vite和Webpack等现代前端构建工具，擅长进行项目工程化配置，以提高开发效率和项目质量.
+        <p mt2 text-base lh-8 italic op50>
+          熟练使用 Vite 和 Webpack 等现代前端构建工具，擅长进行项目工程化配置。能够根据项目需求定制 Webpack 配置，包括代码分割、Tree Shaking、模块热替换（HMR）等，以提高开发效率和项目质量。了解 Babel 和 TypeScript 的使用，并能够进行相应的配置和优化。
         </p>
       </div>
       <div mt-8>
         <h3 text-2xl font-bold>
           Vue
         </h3>
-        <p mt2 text-base italic op50>
-          熟练使用vue 全家桶进行项目开发，vue-router，pinia，axios，unocss，vite的使用.
+        <p mt2 text-base lh-8 italic op50>
+          熟练使用 Vue 全家桶进行项目开发，包括 Vue Router、Pinia、Axios、Unocss、Vite 等。能够实现复杂的路由管理和状态管理，熟悉组件间通信和数据流转。掌握 Vue 3 的 Composition API 和 Vue 2 的 Options API，并能够进行性能优化和代码拆分。
         </p>
       </div>
     </div>
